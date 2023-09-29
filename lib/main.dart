1d83cd14aa02bdcc632cf866155f588e00109454
denn <shuvalov@gmail.com>
2023-09-29 16:10:12 +0300

    Task 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,175 +1,204 @@
-import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
+
+  runApp(MaterialApp(home: new MyApp()));
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class MyApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyAppState createState() => new _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
-  late VideoPlayerController _videoPlayerController;
+class _MyAppState extends State<MyApp> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  late PullToRefreshController pullToRefreshController;
+  Uri url = Uri();
+  double progress = 0;
+  final urlController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _videoPlayerController = VideoPlayerController.networkUrl(
-        Uri.parse(
-            'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'),
-        videoPlayerOptions: VideoPlayerOptions())
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {
-          _videoPlayerController.play();
-        });
-      });
-    _videoPlayerController.setLooping(true);
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
   }
 
   @override
   void dispose() {
-    _videoPlayerController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    int maxBuffering = 0;
-    for (final DurationRange range in _videoPlayerController.value.buffered) {
-      final int end = range.end.inMilliseconds;
-      if (end > maxBuffering) {
-        maxBuffering = end;
-      }
-    }
-    StreamBuilder<Duration?> _progressBar() {
-      return StreamBuilder<Duration?>(
-        stream: _videoPlayerController.position.asStream(),
-        builder: (context, snapshot) {
-          final int duration =
-              _videoPlayerController.value.duration.inMilliseconds;
-          final int position =
-              _videoPlayerController.value.position.inMilliseconds;
-          final progress = position / duration;
-          final buffered = maxBuffering / duration;
-          final total = _videoPlayerController.value.duration;
-          return ProgressBar(
-            progress: Duration(milliseconds: progress.round()),
-            buffered: Duration(milliseconds: buffered.round()),
-            total: total,
-            onDragUpdate: (details) {
-              debugPrint('${details.timeStamp}, ${details.localPosition}');
-            },
-          );
-        },
-      );
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              _videoPlayerController.value.isInitialized
-                  ? AspectRatio(
-                      aspectRatio: _videoPlayerController.value.aspectRatio,
-                      child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: <Widget>[
-                          VideoPlayer(_videoPlayerController),
-                          _progressBar(),
-                        ],
-                      ),
-                    )
-                  : Container(),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    fixedSize: MaterialStateProperty.all(const Size(70, 70)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100)))),
-                onPressed: () {
-                  _videoPlayerController.seekTo(
-                    Duration(
-                        seconds:
-                            _videoPlayerController.value.position.inSeconds -
-                                10),
-                  );
-                },
-                child: const Icon(Icons.replay_10),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _videoPlayerController.pause();
-                },
-                child: const Icon(Icons.pause),
-              ),
-              const Padding(padding: EdgeInsets.all(2)),
-              ElevatedButton(
-                onPressed: () {
-                  _videoPlayerController.play();
-                },
-                child: const Icon(Icons.play_arrow),
-              ),
-              ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    fixedSize: MaterialStateProperty.all(const Size(70, 70)),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                    ),
+      appBar: AppBar(title: const Text("Official InAppWebView website")),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(prefixIcon: Icon(Icons.search)),
+              controller: urlController,
+              keyboardType: TextInputType.url,
+              onSubmitted: (value) {
+                var url = Uri.parse(value);
+                if (url.scheme.isEmpty) {
+                  url = Uri.parse("https://www.google.com/search?q=" + value);
+                }
+                webViewController?.loadUrl(urlRequest: URLRequest(url: url));
+              },
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  InAppWebView(
+                    key: webViewKey,
+                    initialUrlRequest:
+                        URLRequest(url: Uri.parse("https://inappwebview.dev/")),
+                    initialOptions: options,
+                    pullToRefreshController: pullToRefreshController,
+                    onWebViewCreated: (controller) {
+                      webViewController = controller;
+                    },
+                    onLoadStart: (controller, url) {
+                      setState(() {
+                        this.url = url!;
+                        urlController.text = this.url.toString();
+                      });
+                    },
+                    androidOnPermissionRequest:
+                        (controller, origin, resources) async {
+                      return PermissionRequestResponse(
+                          resources: resources,
+                          action: PermissionRequestResponseAction.GRANT);
+                    },
+                    shouldOverrideUrlLoading:
+                        (controller, navigationAction) async {
+                      var uri = navigationAction.request.url!;
+
+                      if (![
+                        "http",
+                        "https",
+                        "file",
+                        "chrome",
+                        "data",
+                        "javascript",
+                        "about"
+                      ].contains(uri.scheme)) {
+                        if (await canLaunchUrl(url)) {
+                          // Launch the App
+                          await launchUrl(
+                            url,
+                          );
+                          // and cancel the request
+                          return NavigationActionPolicy.CANCEL;
+                        }
+                      }
+
+                      return NavigationActionPolicy.ALLOW;
+                    },
+                    onLoadStop: (controller, url) async {
+                      pullToRefreshController.endRefreshing();
+                      setState(() {
+                        this.url = url!;
+                        urlController.text = this.url.toString();
+                      });
+                    },
+                    onLoadError: (controller, url, code, message) {
+                      pullToRefreshController.endRefreshing();
+                    },
+                    onProgressChanged: (controller, progress) {
+                      if (progress == 100) {
+                        pullToRefreshController.endRefreshing();
+                      }
+                      setState(() {
+                        this.progress = progress / 100;
+                        urlController.text = this.url.toString();
+                      });
+                    },
+                    onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                      setState(() {
+                        this.url = url!;
+                        urlController.text = this.url.toString();
+                      });
+                    },
+                    onConsoleMessage: (controller, consoleMessage) {
+                      print(consoleMessage);
+                    },
                   ),
+                  progress < 1.0
+                      ? LinearProgressIndicator(value: progress)
+                      : Container(),
+                ],
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Icon(Icons.arrow_back),
                   onPressed: () {
-                    _videoPlayerController.seekTo(
-                      Duration(
-                        seconds:
-                            _videoPlayerController.value.position.inSeconds +
-                                10,
-                      ),
-                    );
+                    webViewController?.goBack();
                   },
-                  child: const Icon(Icons.forward_10))
-            ],
-          )
-        ],
+                ),
+                ElevatedButton(
+                  child: const Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    webViewController?.goForward();
+                  },
+                ),
+                progress < 1.0
+                    ? ElevatedButton(
+                        child: const Icon(Icons.close),
+                        onPressed: () {
+                          webViewController?.stopLoading();
+                        },
+                      )
+                    : ElevatedButton(
+                        child: const Icon(Icons.refresh),
+                        onPressed: () {
+                          webViewController?.reload();
+                        },
+                      ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
